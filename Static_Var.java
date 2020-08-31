fed345004a8b04bed3e7358becdbbde24a39db0b
MMeghana <63573802+MMeghana@users.noreply.github.com>
2020-08-31 14:53:40 +0530

    Add files via upload

---
+++ Static_Var.java
@@ -0,0 +1,14 @@
+package Pack1;
+public class Static_Var 
+{
+      	static int x =10;
+	public static void main(String[] args) 
+	{
+	   Static_Var t1 = new Static_Var();
+	   t1.x=50;
+	   System.out.println(t1.x);  //50
+	   
+	   Static_Var t2 = new Static_Var();
+	   System.out.println(t2.x);  //50  
+	}
+}
